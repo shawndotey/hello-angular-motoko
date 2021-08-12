000ede435c7be03ffa80d9e8ccd216dc598f0900
phkla1 <phkla1@localhost>
2021-08-12 12:21:39 +0100

    Added angular logo to assets Reverted app.html to dfinity format, adding angular logo Adjusted title in app.html Reverted app.scss to dfinity format. Added logo-container class Adjusted app.ts to take user input and echo back to frontend (changed getResponse())

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,19 @@ export class AppComponent {
   public title = 'hello-angular-motoko';
   public response = '';
   constructor(private helloService: IcHelloService){
-    this.getResponse();
+//    this.getResponse();
   }
+  /*
   public async getResponse(){
     this.response = await this.helloService.greet('Angular');
   }
+  */
+
+	public async getResponse(){
+		let name = (document.getElementById('name') as HTMLInputElement)?.value;
+		let greeting = document.getElementById('greeting');
+		this.response = await this.helloService.greet(name);
+		greeting ?  greeting.innerText = this.response : Function.prototype();
+
+	}
 }
